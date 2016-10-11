0aee1ce20352e98a92c58ece8e0a24cd1f43615a
logiclogue <jordanpaullord@hotmail.co.uk>
2016-10-11 19:33:00 +0100

    Adds Particle class

---
+++ src/Particle.c
@@ -0,0 +1,14 @@
+#include "Particle.h"
+
+
+Particle *Particle_main(float mass)
+{
+    Particle *self = malloc(sizeof(Particle));
+
+    self->x = 0;
+    self->y = 0;
+    self->mass = mass;
+    self->radius = 0;
+
+    return self;
+}
---
+++ src/Particle.h
@@ -0,0 +1,12 @@
+#ifndef _PARTICLE_H_
+#define _PARTICLE_H_
+
+
+typedef struct ParticleTag {
+    float x;
+    float y;
+    float mass;
+    float radius;
+} Particle;
+
+#endif
