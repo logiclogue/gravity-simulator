07834a06e1be3cd2a44dca3184df5baf565745b0
logiclogue <jordanpaullord@hotmail.co.uk>
2016-10-13 22:22:57 +0100

    Updates method declarations

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -7,6 +7,14 @@
 typedef struct LinkedListTag {
     LinkedListNode *first_node;
     LinkedListNode *last_node;
+    LinkedListNode *current_node;
 } LinkedList;
+
+
+LinkedList *LinkedList_new(void);
+void *LinkedList_add(LinkedList *self, LinkedListNode *node);
+void *LinkedList_remove(LinkedList *self, LinkedListNode *node);
+LinkedListNode *LinkedList_next(LinkedList *self);
+LinkedListNode *LinkedList_jump(LinkedList *self, LinkedListNode *node);
 
 #endif
