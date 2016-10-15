b054420fa11c0325539aeee40f1e5906c9b2634f
logiclogue <jordanpaullord@hotmail.co.uk>
2016-10-15 20:32:07 +0100

    Updates demo

--- src/main.c
+++ src/main.c
@@ -2,11 +2,8 @@
 #include "Drawer.h"
 #include "Frame.h"
 #include "Camera.h"
-#include "Triangle.h"
 #include "Circle.h"
-
-
-static void create_triangles(Triangle *top, Triangle *bottom);
+#include "Universe.h"
 
 
 int main(void)
@@ -14,22 +11,17 @@ int main(void)
     Camera *camera = Camera_new();
     Frame *frame = Frame_new(camera);
     Drawer *drawer = Drawer_new(camera, frame);
-    Triangle *triangle_top = Triangle_new(drawer);
-    Triangle *triangle_bottom = Triangle_new(drawer);
     Circle *circle = Circle_new(drawer);
+    Universe *universe = Universe_new();
 
     float x = 0;
 
-    create_triangles(triangle_top, triangle_bottom);
+    Universe_destroy(universe);
 
     while (!frame->is_exit) {
         camera->y = 0.2 * x;
 
-        triangle_top->x = triangle_bottom->x = x;
-        Triangle_draw(triangle_top);
-        Triangle_draw(triangle_bottom);
         Circle_draw(circle);
-
         Frame_draw(frame);
         Drawer_clear(drawer);
         Frame_event_loop(frame);
@@ -40,20 +32,4 @@ int main(void)
     }
 
     return 0;
-}
-
-
-static void create_triangles(Triangle *top, Triangle *bottom)
-{
-    top->ax = 0.f;
-    top->ay = 1.f;
-
-    top->bx = bottom->bx = -1.f;
-    top->by = bottom->by = 0.f;
-
-    top->cx = bottom->cx = 1.f;
-    top->cy = bottom->cy = 0.f;
-
-    bottom->ax = 0.f;
-    bottom->ay = -1.f;
 }
