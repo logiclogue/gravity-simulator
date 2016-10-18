13d062673fd9f2421dde22b3229eb7d564f4322e
logiclogue <jordanpaullord@hotmail.co.uk>
2016-10-18 18:51:35 +0100

    Tweaks

--- src/Universe.h
+++ src/Universe.h
@@ -11,8 +11,8 @@ typedef struct UniverseTag {
 
 
 Universe *Universe_new(void);
-void Universe_add(Universe *self, Particle *particle);
 void Universe_destroy(Universe *self);
+void Universe_add(Universe *self, Particle *particle);
 void Universe_test(Universe *self);
 
 #endif
