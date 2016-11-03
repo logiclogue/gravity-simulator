2c88604cc57583f332674c82c242abe33fc8048a
logiclogue <jordanpaullord@hotmail.co.uk>
2016-11-03 21:27:54 +0000

    Updates gravity to sort of work

--- src/Gravity.c
+++ src/Gravity.c
@@ -17,13 +17,16 @@ float Gravity_force(Gravity *self,
 {
     float mass_a = particle_a->mass;
     float mass_b = particle_b->mass;
-    float distance_square = distance * distance;
+    float distance_square = distance * abs(distance);
+    float force;
 
     if (distance_square == 0) {
         return 0;
     }
+    
+    force = (self->constant * mass_a * mass_b) / distance_square;
 
-    return (self->constant * mass_a * mass_b) / distance_square;
+    return force;
 }
 
 float Gravity_force_x(Gravity *self,
